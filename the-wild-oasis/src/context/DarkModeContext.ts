a61d97065643cb21246f349d544ed2ef9bbcf89a
Ahmed-Magdy28 <ahmedmagdy2849@gmail.com>
2026-03-30 22:50:29 +0200

    Remove the-wild-oasis folder from tracking (preparing for submodule)

--- the-wild-oasis/src/context/DarkModeContext.ts
+++
@@ -1,9 +0,0 @@
-import { createContext } from 'react';
-
-export const DarkModeContext = createContext<
-   | {
-        isDarkMode: boolean;
-        toggleDarkMode: () => void;
-     }
-   | undefined
->(undefined);
--- the-wild-oasis/src/ui/StyledApp.tsx
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components';
-
-const StyledApp = styled.main`
-   /* background-color: orangered; */
-   padding: 20px;
-`;
-export default StyledApp;
